199da160209b8d424d03761b4317c59e7c2acca8
37hashiguchi <tgif3773@gmail.com>
2021-02-01 14:50:18 +0900

    初回コミット

--- js/toppage.js
+++ js/toppage.js
@@ -17,3 +17,15 @@ $(function() {
         return false;
     });
 });
+
+var navPos = jQuery( '#global-nav' ).offset().top; // グローバルメニューの位置
+var navHeight = jQuery( '#global-nav' ).outerHeight(); // グローバルメニューの高さ
+jQuery( window ).on( 'scroll', function() {
+  if ( jQuery( this ).scrollTop() > navPos ) {
+    jQuery( 'body' ).css( 'padding-top', navHeight );
+    jQuery( '#global-nav' ).addClass( 'm_fixed' );
+  } else {
+    jQuery( 'body' ).css( 'padding-top', 0 );
+    jQuery( '#global-nav' ).removeClass( 'm_fixed' );
+  }
+});
