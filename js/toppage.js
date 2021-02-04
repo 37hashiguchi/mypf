8dd0588ddf08ea416769d6822007d0b9010d6358
37hashiguchi <tgif3773@gmail.com>
2021-02-04 20:16:43 +0900

    changed to wire cursor

--- js/toppage.js
+++ js/toppage.js
@@ -1,3 +1,5 @@
+
+
 // スライダー---------------------------------------------
 $('.slider-for').slick({
 slidesToShow: 1,
@@ -47,54 +49,45 @@ topBtn.click(function () {
 });
 });
 //カーソル-------------------------------------------
-var
-cursor = $(".cursor"),
-follower = $(".follower"),
-cWidth = 8, //カーソルの大きさ
-fWidth = 40, //フォロワーの大きさ
-delay = 10, //数字を大きくするとフォロワーがより遅れて来る
-mouseX = 0, //マウスのX座標
-mouseY = 0, //マウスのY座標
-posX = 0, //フォロワーのX座標
-posY = 0; //フォロワーのX座標
+// CURSOR
+var cursor = $(".cursor"),
+follower = $(".cursor-follower");
 
-//カーソルの遅延アニメーション
-//ほんの少ーーーしだけ遅延させる 0.001秒
-TweenMax.to({}, .001, {
+var posX = 0,
+    posY = 0;
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.01, {
   repeat: -1,
   onRepeat: function() {
-    posX += (mouseX - posX) / delay;
-    posY += (mouseY - posY) / delay;
-    
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
     TweenMax.set(follower, {
-        css: {    
-          left: posX - (fWidth / 2),
-          top: posY - (fWidth / 2)
+        css: {
+        left: posX - 36,
+        top: posY - 40
         }
     });
-    
     TweenMax.set(cursor, {
-        css: {    
-          left: mouseX - (cWidth / 2),
-          top: mouseY - (cWidth / 2)
+        css: {
+        left: mouseX,
+        top: mouseY
         }
     });
   }
 });
 
-//マウス座標を取得
 $(document).on("mousemove", function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 });
-
-$("a").on({
-  "mouseenter": function() {
-    cursor.addClass("is-active");
-    follower.addClass("is-active");
-  },
-  "mouseleave": function() {
-    cursor.removeClass("is-active");
-    follower.removeClass("is-active");
-  }
+// yellow circle
+$(".link").on("mouseenter", function() {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$(".link").on("mouseleave", function() {
+    cursor.removeClass("active");
+    follower.removeClass("active");
 });
